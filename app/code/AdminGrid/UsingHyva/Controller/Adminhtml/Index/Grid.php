b8ca7bc79faafa3ce5369958dbac65f68e7180ba
Ritesh Rana <13ritesh2001@gmail.com>
2023-04-03 17:33:23 +0530

    Admin-Grid-Using-Hyva-Theme

---
+++ app/code/AdminGrid/UsingHyva/Controller/Adminhtml/Index/Grid.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AdminGrid\UsingHyva\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Grid extends Action
+{
+
+    public $pageFactory;
+    public $context;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->set('Hyva Grid');
+        return $resultPage;
+    }
+}
---
+++ app/code/AdminGrid/UsingHyva/registration.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @package        Ritesh_Rana
+ * @author         Ritesh Rana
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'AdminGrid_UsingHyva',
+    __DIR__
+);
